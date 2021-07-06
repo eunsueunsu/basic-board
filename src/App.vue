9ac8db6bc944431103936e16d1d41c3b482d4f92
eunsueunsu <imimim93@gmail.com>
2021-07-06 14:11:17 +0900

    add darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,41 @@
 <template>
-  <v-app>
+  <v-app >
     <v-app-bar
       app
       color="primary"
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <h2>say HI</h2>
+<!--        <v-img-->
+<!--          alt="Vuetify Logo"-->
+<!--          class="shrink mr-2"-->
+<!--          contain-->
+<!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"-->
+<!--          transition="scale-transition"-->
+<!--          width="40"-->
+<!--        />-->
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+<!--        <v-img-->
+<!--          alt="Vuetify Name"-->
+<!--          class="shrink mt-1 hidden-sm-and-down"-->
+<!--          contain-->
+<!--          min-width="100"-->
+<!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"-->
+<!--          width="100"-->
+<!--        />-->
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-switch :label="`Dark Theme`" v-model="$vuetify.theme.dark"></v-switch>
+<!--      <v-btn-->
+<!--        href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
+<!--        target="_blank"-->
+<!--        text-->
+<!--      >-->
+<!--        <span class="mr-2">Latest Release</span>-->
+<!--        <v-icon>mdi-open-in-new</v-icon>-->
+<!--      </v-btn>-->
     </v-app-bar>
 
     <v-main>
@@ -54,6 +55,7 @@ export default {
   },
 
   data: () => ({
+
     //
   }),
 };
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,19 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+const vuetify = new Vuetify({
+    theme:{
+        themes:{
+            light:{
+                primary : '#ff80ab',
+                secondary : '#81c784'
+            },
+            dark:{
+                primary:'#c94f7c',
+
+
+            }
+        }
+    }
+})
+export default vuetify
