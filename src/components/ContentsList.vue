ab309edd2b4af3bf5221404833a9987c6a23a05f
eunsueunsu <imimim93@gmail.com>
2021-07-07 18:00:58 +0900

    add textarea

--- src/components/ContentsList.vue
+++ src/components/ContentsList.vue
@@ -5,7 +5,15 @@
   <v-col>
 <!--    // input-->
     <div>
-<input v-model="message" placeholder="이름">
+<v-textarea label="닉네임" outlined rows="1" row-height="10">
+</v-textarea>
+<v-textarea label="비밀번호" outlined rows="1" row-height="10">
+</v-textarea>
+    </div>
+    <div>
+      <v-textarea label="예쁜말 고운말" outlined rows="1" row-height="10">
+      </v-textarea>
+
     </div>
   </v-col>
 </v-row>
