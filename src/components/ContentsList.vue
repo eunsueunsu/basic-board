0043762148625ff3210f0345b91379749637a994
eunsueunsu <imimim93@gmail.com>
2021-07-13 15:09:32 +0900

    add v-card

--- src/components/ContentsList.vue
+++ src/components/ContentsList.vue
@@ -1,9 +1,22 @@
 <template>
   <v-container>
     <v-list>
-      <div v-for="data in list" :key="data.id">
-        <p>{{data.content}}</p>
-      </div>
+      <v-card elevation="2" outlined rounded v-for="data in list" :key="data.id">
+        <v-card-subtitle>{{ data.name }}</v-card-subtitle>
+        <v-card-subtitle>{{ data.content }}</v-card-subtitle>
+        <v-card-actions>
+          <v-btn small rounded outlined class="secondary">
+            <v-icon>mdi-pencil</v-icon>
+
+          </v-btn>
+          <v-btn small rounded outlined class="secondary">
+            <v-icon>
+              mdi-delete
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+
     </v-list>
   </v-container>
 
@@ -12,10 +25,13 @@
 <script>
 export default {
   name: "ContentsList",
-  data : ()=> ({
+  data: () => ({
     list: [
-      {id :1, content : "test1"},
-      {id :2, content : "test2"},
+      {id: 1, name: "eunsu lee", content: "test1 "},
+      {id: 2, name: "eunsu lee", content: "test2"},
+      {id: 3, name: "eunsu lee", content: "test2"},
+      {id: 4, name: "eunsu lee", content: "test2"},
+      {id: 5, name: "eunsu lee", content: "test2"},
     ]
   })
 }
