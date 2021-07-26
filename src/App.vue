9360812b8d0c0c78752b1d755108dd07ad7bd464
eunsueunsu <imimim93@gmail.com>
2021-07-26 22:02:15 +0900

    fix menu

--- src/App.vue
+++ src/App.vue
@@ -55,21 +55,44 @@
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list>
         <v-list-item-group
-        active-class="primary--text text--accent-4">
-        <v-list-item>
-          <v-list-item-icon>
-          <v-icon large>
-            mdi-github
-          </v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>
-            github
-          </v-list-item-title>
-        </v-list-item>
+            active-class="primary--text text--accent-4">
           <v-list-item>
-            <v-switch :label="`깜깜하게`" v-model="$vuetify.theme.dark" class="float-right"></v-switch>
+            <v-list-item-icon>
+              <v-icon large>
+                mdi-github
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              Github
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon large>
+                mdi-account
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              Resume
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-switch :label="`dark mode`" v-model="$vuetify.theme.dark" class="float-right"></v-switch>
 
           </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon large>
+                mdi-login
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              login as admin
+            </v-list-item-title>
+          </v-list-item>
+
         </v-list-item-group>
       </v-list>
 
